c292d99c5451ac15fb03db4b319f3f58f6dfaf9d
KodFazil <57687767+KodFazil@users.noreply.github.com>
2021-10-24 00:00:48 +0300

    Add files via upload

---
+++ BigSorting/Main.java
@@ -0,0 +1,29 @@
+package BigSorting;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {            // 23 Ekim 2021 < Fazıl >
+
+    static String [] bigSorting(String [] arr) {
+        Arrays.sort(arr, (left, right) -> {
+            if (left.length() != right.length())
+                return left.length() - right.length();
+            return left.compareTo(right);
+        });
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        String [] array = new String[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.next();
+        }
+        String [] arr = bigSorting(array);
+        for (int j = 0; j < n; j++) {
+            System.out.println(arr[j]);
+        }
+    }
+}
