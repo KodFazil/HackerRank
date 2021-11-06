f3ff0d4b633818528245305c878b882d42017d47
KodFazil <57687767+KodFazil@users.noreply.github.com>
2021-11-06 17:19:51 +0300

    Add files via upload

---
+++ CountingSort1/Main.java
@@ -0,0 +1,22 @@
+package CountingSort1;
+
+import java.util.Scanner;
+
+public class Main {       // 6 Ekim 2021 < Fazıl >
+
+    static int[] countingSort(int[] array) {
+        int [] frequencyArray = new int[100];
+        for (int i = 0; i < array.length; i++) frequencyArray[array[i]]++;
+        return frequencyArray;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int [] array = new int[n];
+        for (int i = 0; i < n; i++) array[i] = scanner.nextInt();
+        int [] result = countingSort(array);
+        for (int values : result) System.out.print(values + " ");
+    }
+
+}
